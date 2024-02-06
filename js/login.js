a0624472a1c6c91fe851672a33c1d1d3447ab7c0
Ahmed <S10258532@connect.np.edu.sg>
2024-02-06 19:33:21 +0800

    did some changes to navigation, the design

--- js/login.js
+++ js/login.js
@@ -1,71 +1,79 @@
-
 (function ($) {
-    "use strict";
+  "use strict";
 
-
-     /*==================================================================
+  /*==================================================================
     [ Focus input ]*/
-    $('.input100').each(function(){
-        $(this).on('blur', function(){
-            if($(this).val().trim() != "") {
-                $(this).addClass('has-val');
-            }
-            else {
-                $(this).removeClass('has-val');
-            }
-        })    
-    })
-  
-  
-    /*==================================================================
+  $(".input100").each(function () {
+    $(this).on("blur", function () {
+      if ($(this).val().trim() != "") {
+        $(this).addClass("has-val");
+      } else {
+        $(this).removeClass("has-val");
+      }
+    });
+  });
+
+  /*==================================================================
     [ Validate ]*/
-    var input = $('.validate-input .input100');
+  var input = $(".validate-input .input100");
 
-    $('.validate-form').on('submit',function(){
-        var check = true;
+  $(".validate-form").on("submit", function (event) {
+    event.preventDefault(); // Prevent the default form submission
 
-        for(var i=0; i<input.length; i++) {
-            if(validate(input[i]) == false){
-                showValidate(input[i]);
-                check=false;
-            }
-        }
+    var check = true;
 
-        return check;
+    for (var i = 0; i < input.length; i++) {
+      if (validate(input[i]) == false) {
+        showValidate(input[i]);
+        check = false;
+      }
+    }
+
+    if (check) {
+      redirectToHomePage(); // If form validation is successful, redirect to home page
+    }
+  });
+
+  $(".validate-form .input100").each(function () {
+    $(this).focus(function () {
+      hideValidate(this);
     });
+  });
 
-
-    $('.validate-form .input100').each(function(){
-        $(this).focus(function(){
-           hideValidate(this);
-        });
-    });
-
-    function validate (input) {
-        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
-            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
-                return false;
-            }
-        }
-        else {
-            if($(input).val().trim() == ''){
-                return false;
-            }
-        }
+  function validate(input) {
+    if ($(input).attr("type") == "email" || $(input).attr("name") == "email") {
+      if (
+        $(input)
+          .val()
+          .trim()
+          .match(
+            /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/
+          ) == null
+      ) {
+        return false;
+      }
+    } else {
+      if ($(input).val().trim() == "") {
+        return false;
+      }
     }
+    return true;
+  }
 
-    function showValidate(input) {
-        var thisAlert = $(input).parent();
+  function showValidate(input) {
+    var thisAlert = $(input).parent();
 
-        $(thisAlert).addClass('alert-validate');
-    }
+    $(thisAlert).addClass("alert-validate");
+  }
 
-    function hideValidate(input) {
-        var thisAlert = $(input).parent();
+  function hideValidate(input) {
+    var thisAlert = $(input).parent();
 
-        $(thisAlert).removeClass('alert-validate');
-    }
-    
-    
+    $(thisAlert).removeClass("alert-validate");
+  }
 
+  function redirectToHomePage() {
+    // Redirect the user to the home page
+    window.location.href = "index.html";
+  }
 })(jQuery);
